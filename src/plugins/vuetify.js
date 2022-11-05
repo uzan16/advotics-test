8eb58be4d9444fb88d2ca32ddc1051f83d1e89da
Fauzan Adhima Alwan <fauzan.adhima.alwan@gmail.com>
2022-11-05 11:15:32 +0700

    Initial Commit

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,16 +11,7 @@ export default new Vuetify({
     themes: {
       light: {
         primary: "#00A551",
-        // primaryDark: "#0D47A1",
         secondary: "#D1EDDB"
-        // accent: "#00BCD4",
-        // error: "#B71C1C",
-        // info: "#2196F3",
-        // success: "#4CAF50",
-        // warning: "#FF8800",
-        // progress: "#E6EE9C",
-        // orange: "#D97D46",
-        // tertiary: "#345345"
       }
     }
   }
